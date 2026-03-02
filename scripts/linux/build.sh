ff64a03eaf7fc967c28c58e95857c47b2cc6ac12
Felipe Xavier <felipeacx@gmail.com>
2026-03-02 15:10:06 +0100

    Fixed linux build script

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -22,6 +22,7 @@ QT_VERSION="$(tr -d ' \n' < $CURR_DIR/.qt-version)"
 BUILD_PATH="$SOURCE_DIR/build/linux/debug"
 PREFIX_PATH="$HOME/Qt/$QT_VERSION/gcc_64"
 INSTALL_PREFIX="$SOURCE_DIR/release/linux"
+EXTRA_ARGS=""
 
 while [[ $# -gt 0 ]]; do
   key="$1"
@@ -50,9 +51,13 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
-      *)
+      --help)
       printHelp $0
-      exit 1
+      exit 0
+      ;;
+      *)
+      EXTRA_ARGS="${EXTRA_ARGS} $1"
+      shift
       ;;
   esac
 done
@@ -66,6 +71,7 @@ echo "  TARGET: ${TARGET}"
 echo "  BUILD_PATH: ${BUILD_PATH}"
 echo "  PREFIX_PATH: ${PREFIX_PATH}"
 echo "  INSTALL_PREFIX: ${INSTALL_PREFIX}"
+echo "  Extra args: ${EXTRA_ARGS}"
 echo "--------------------------------------"
 
 # ----------------------------------
@@ -81,7 +87,8 @@ else
     -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
     -DLOCAL_QT_PATH="$LOCAL_QT_PATH" \
     -DLOCAL_PROJECT_PATH="$LOCAL_PROJECT_PATH" \
-    -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
+    -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+    $EXTRA_ARGS
 
   if [ "$BUILD_TYPE" == "Release" ]; then
     cmake --build "$BUILD_PATH" -j 4 --target deploy-linux
