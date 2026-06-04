437ccd125ea5950c7f0f0a42a25cbe66fb432f6e
Felipe Xavier <felipeacx@gmail.com>
2026-06-04 17:19:35 +0200

    Fixed maki_to_koda for separated names

--- app/widgets/properties/properties_menu.cpp
+++ app/widgets/properties/properties_menu.cpp
@@ -443,7 +443,7 @@ VoidResult PropertiesMenu::loadPropertyComponentSelect(const PropertyInfo& prope
     {
       if (option->gettype() == Types::PropertyTypes::EVENT_SELECT)
       {
-        LOG_WARN_ON_FAILURE(loadFieldEventSelect(widget, option->getid(), property, node, [this](const QString& nodeId, QComboBox* eventWidget) {
+        LOG_WARN_ON_FAILURE(loadFieldEventSelect(widget, option->getid(), property, node, Types::CallType::UNKNOWN, [this](const QString& nodeId, QComboBox* eventWidget) {
           eventWidget->clear();
           auto events = mStorage->getEventsOfTypeFromNode(nodeId, {Types::CallType::TRIGGER, Types::CallType::ABORT, Types::CallType::IN});
           for (const auto& event : events)
@@ -457,7 +457,7 @@ VoidResult PropertiesMenu::loadPropertyComponentSelect(const PropertyInfo& prope
       }
       else if (option->gettype() == Types::PropertyTypes::FLOW_CALL)
       {
-        LOG_WARN_ON_FAILURE(loadFieldEventSelect(widget, option->getid(), property, node, [this](const QString& nodeId, QComboBox* eventWidget) {
+        LOG_WARN_ON_FAILURE(loadFieldEventSelect(widget, option->getid(), property, node, Types::CallType::USER, [this](const QString& nodeId, QComboBox* eventWidget) {
           eventWidget->clear();
           auto events = mStorage->getFlowsFromNode(nodeId);
           for (const auto& event : events)
@@ -473,7 +473,7 @@ VoidResult PropertiesMenu::loadPropertyComponentSelect(const PropertyInfo& prope
   return VoidResult();
 }
 
-VoidResult PropertiesMenu::loadFieldEventSelect(maki::SelectorWidget* componentSelect, const QString& optionId, const PropertyInfo& property, NodeItem* node,
+VoidResult PropertiesMenu::loadFieldEventSelect(maki::SelectorWidget* componentSelect, const QString& optionId, const PropertyInfo& property, NodeItem* node, Types::CallType callType,
                                                 std::function<void(const QString& nodeId, QComboBox* eventWidget)> populate)
 {
   QComboBox* eventCombo = new QComboBox(this);
@@ -495,7 +495,7 @@ VoidResult PropertiesMenu::loadFieldEventSelect(maki::SelectorWidget* componentS
       widget->setValue(currentEvent);
 
       // Finally, based on the event, we can set the arguments
-      LOG_WARN_ON_FAILURE(loadEventArguments(currentComponentId, currentEvent, property, node, Types::CallType::UNKNOWN, group));
+      LOG_WARN_ON_FAILURE(loadEventArguments(currentComponentId, currentEvent, property, node, callType, group));
     }
     else
     {
@@ -504,13 +504,13 @@ VoidResult PropertiesMenu::loadFieldEventSelect(maki::SelectorWidget* componentS
   }
 
   // When the event changes
-  connect(widget, &maki::SelectorWidget::valueChanged, this, [this, node, property, componentSelect, group](const QString& eventName) {
+  connect(widget, &maki::SelectorWidget::valueChanged, this, [this, node, property, componentSelect, group, callType](const QString& eventName) {
     if (eventName.isEmpty())
       return;
 
     clearLayout(group->layout());
     UPDATE_PROPERTY_ARG(node, property.getid(), EVENT_INDEX, eventName, Types::PropertyTypes::EVENT_SELECT, false)
-    LOG_WARN_ON_FAILURE(loadEventArguments(componentSelect->getData().toString(), eventName, property, node, Types::CallType::UNKNOWN, group));
+    LOG_WARN_ON_FAILURE(loadEventArguments(componentSelect->getData().toString(), eventName, property, node, callType, group));
 
     // Update the node name
     updateBlockName(node, componentSelect->getValue(), eventName);
@@ -558,9 +558,15 @@ VoidResult PropertiesMenu::loadFieldTriggerCall(maki::SelectorWidget* componentS
 VoidResult PropertiesMenu::loadEventArguments(const QString& nodeId, const QString& flowName, const PropertyInfo& property, NodeItem* node, Types::CallType callType, maki::WidgetGroup* group)
 {
   std::shared_ptr<FlowSaveInfo> event = nullptr;
-  if (callType == Types::CallType::UNKNOWN)
+  if (callType == Types::CallType::USER)
   {
     event = mStorage->getFlowFromNode(nodeId, flowName);
+  }
+  else if (callType == Types::CallType::UNKNOWN)
+  {
+    auto events = mStorage->getEventsFromNode(nodeId);
+    if (!events.isEmpty())
+      event = events.first();
   }
   else
   {
