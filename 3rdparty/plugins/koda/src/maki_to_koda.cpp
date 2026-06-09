8f98bfd5faaac43ea28919b62d693a61989b1da4
JipesNl <18496900+JipesNl@users.noreply.github.com>
2026-06-09 21:05:43 +0200

    Add unique ID to component name

--- 3rdparty/plugins/koda/src/maki_to_koda.cpp
+++ 3rdparty/plugins/koda/src/maki_to_koda.cpp
@@ -149,7 +149,9 @@ Result<koda::PComponent> MakiToKoda::buildCapability(const INode& capability)
     return Result<koda::PComponent>::Failed("Capabiity does not have a name");
 
   auto name = properties["name"].toString();
-  c->name = format(name);
+  std::string formattedName = format(name);
+  std::string uniqueID = generateUniqueId();
+  c->name = formattedName + "_" + uniqueID;
 
   auto typeArray = properties["type"].toJsonObject()["options"].toArray();
   if (typeArray.isEmpty())
