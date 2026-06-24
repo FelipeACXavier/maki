49b2616467ea38377796bc3762133dd8c0cc144b
JipesNl <18496900+JipesNl@users.noreply.github.com>
2026-06-24 05:19:40 +0200

    Fixes with pointer handling

--- 3rdparty/plugins/koda/src/ast/koda_compiler.cpp
+++ 3rdparty/plugins/koda/src/ast/koda_compiler.cpp
@@ -1053,7 +1053,7 @@ Result<ReturnValue> Compiler::generateStrategyHandler(PStrategyHandler handler, 
     env.requiresPorts.insert("isignal " + expr.name);
 
     ReturnValue strat;
-    ASSIGN_OR_RETURN_ON_FAILURE(strat, generateStrategy(handler->body, **safeChild(node, "body", 0), env));
+    ASSIGN_OR_RETURN_ON_FAILURE(strat, generateStrategy(handler->body, *safeChild(node, "body", 0), env));
 
     env.core.push_back(std::format("sh{}.signal <=> {}", id, expr.name));
     env.core.push_back(std::format("sh{}.action <=> {}", id, env.previousCall));
